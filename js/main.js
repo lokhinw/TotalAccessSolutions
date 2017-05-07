e04db6e5b7d7bb73484eeb2bf1e4b9dd86dea0c1
Jusitn Martin <justinmartin2001@outlook.com>
2017-05-07 13:15:46 -0400

    Completed Navbar / Unfinsihed Navbar

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var img = 0;
+function carousel(){
+    if (img == 0) {
+$(".landing-page").css("background-image","url(img/bg2.jpg)");
+img = 1;
+} else {
+$(".landing-page").css("background-image","url(img/bg1.jpg)");
+img = 0;
+}
+
+    setTimeout(carousel, 3000);
+}
+
+carousel();
