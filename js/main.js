2b9ceb2e4078272804133f4273b65c6ac9784084
Lokhin Wu <lokhin.wu@live.com>
2017-05-07 13:41:48 -0400

    Small Fix

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ var img = 0;
 
 function carousel() {
   if (img === 0) {
-    $(".landing-page").css("background-image", "url(img/bg2.jpg)");
+    $(".landing-page").css("background-image", "url(img/bg1.jpg)");
     img = 1;
   } else {
-    $(".landing-page").css("background-image", "url(img/bg1.jpg)");
+    $(".landing-page").css("background-image", "url(img/bg2.jpg)");
     img = 0;
   }
 
