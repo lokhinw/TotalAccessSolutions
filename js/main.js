ce2c4c37acbc6501d636227f08ee2e95d7208bc3
Lokhin Wu <lokhin.wu@live.com>
2017-05-08 17:08:31 -0400

    Carousel

--- js/main.js
+++ js/main.js
@@ -4,12 +4,15 @@ function carousel() {
   if (img === 0) {
     $(".landing-page").css("background-image", "url(img/bg1.jpg)");
     img = 1;
-  } else {
+  } else if (img == 1) {
     $(".landing-page").css("background-image", "url(img/bg2.jpg)");
+    img = 2;
+  } else if (img == 2) {
+    $(".landing-page").css("background-image", "url(img/bg3.jpg)");
     img = 0;
   }
 
-  setTimeout(carousel, 30000000);
+  setTimeout(carousel, 5000);
 }
 
 carousel();
